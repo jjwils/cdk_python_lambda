07fc178ca0abce81520e9a6eb6c05fcef4878b14
jjwils <mail@johnwilson.me.uk>
2023-08-17 19:26:43 +0100

    intial commit

--- cdk_docker_lambda/cdk_docker_lambda_stack.py
+++ cdk_docker_lambda/cdk_docker_lambda_stack.py
@@ -1,19 +1,25 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack
+from aws_cdk import aws_lambda as _lambda
 from constructs import Construct
 
-class CdkDockerLambdaStack(Stack):
 
+class CdkDockerLambdaStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        self.build_lambda_func()
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkDockerLambdaQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+    def build_lambda_func(self):
+        self.prediction_lambda = _lambda.DockerImageFunction(
+            scope=self,
+            id="ExampleDockerLambda",
+            # Function name on AWS
+            function_name="ExampleDockerLambda",
+            # Use aws_cdk.aws_lambda.DockerImageCode.from_image_asset to build
+            # a docker image on deployment
+            code=_lambda.DockerImageCode.from_image_asset(
+                # Directory relative to where you execute cdk deploy
+                # contains a Dockerfile
+                directory="cdk_docker_lambda/ExampleDockerLambda"
+            ),
+        )
